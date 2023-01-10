03ae1a8ba9467124f846c5c72dded42d9ae25859
anna-luiza-haus <luizasgusmao@hotmail.com>
2023-01-10 11:26:33 -0300

    AlterReactHookForm

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -4,6 +4,7 @@ import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
+import { Controller } from 'react-hook-form'
 
 const newTransactionFormSchema = z.object({
     description: z.string(),
@@ -15,15 +16,19 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionsModal() {
-    const { 
-        register, 
+    const {
+        control,
+        register,
         handleSubmit,
         formState: { isSubmitting }
     } = useForm<NewTransactionFormInputs>({
-        resolver: zodResolver(newTransactionFormSchema)
+        resolver: zodResolver(newTransactionFormSchema),
+        defaultValues: {
+            type: 'income'
+        },
     })
 
-    function handleCreateNewTransaction (data: NewTransactionFormInputs) {
+    function handleCreateNewTransaction(data: NewTransactionFormInputs) {
 
     }
 
@@ -33,39 +38,47 @@ export function NewTransactionsModal() {
             <Content>
                 <Dialog.Title>Nova Transação</Dialog.Title>
                 <CloseButton>
-                    <X size={24}/>
+                    <X size={24} />
                 </CloseButton>
 
                 <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
-                    <input 
-                        type="text" 
-                        placeholder='Descrição' 
+                    <input
+                        type="text"
+                        placeholder='Descrição'
                         required
                         {...register('description')}
                     />
-                    <input 
-                        type="number" 
-                        placeholder='Preço' 
+                    <input
+                        type="number"
+                        placeholder='Preço'
                         required
                         {...register('price', { valueAsNumber: true })}
                     />
-                    <input 
-                        type="text" 
-                        placeholder='Categoria' 
+                    <input
+                        type="text"
+                        placeholder='Categoria'
                         required
                         {...register('category')}
                     />
 
-                    <TransactionType>
-                        <TransactionTypeButton variant='income' value='income'>
-                            <ArrowCircleUp size={24} />
-                            Entrada
-                        </TransactionTypeButton>
-                        <TransactionTypeButton variant='outcome' value='outcome'>
-                            <ArrowCircleDown size={24} />
-                            Saída
-                        </TransactionTypeButton>
-                    </TransactionType>
+                    <Controller
+                        control={control}
+                        name="type"
+                        render={({ field }) => {
+                            return (
+                                <TransactionType onValueChange={field.onChange} value={field.value}>
+                                    <TransactionTypeButton variant='income' value='income'>
+                                        <ArrowCircleUp size={24} />
+                                        Entrada
+                                    </TransactionTypeButton>
+                                    <TransactionTypeButton variant='outcome' value='outcome'>
+                                        <ArrowCircleDown size={24} />
+                                        Saída
+                                    </TransactionTypeButton>
+                                </TransactionType>
+                            )
+                        }}
+                    />
 
                     <button type='submit' disabled={isSubmitting}>Cadastrar</button>
                 </form>
